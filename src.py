d6638e42d623d527aabb121c55af36d88ec83f95
mahy <992635910@qq.com>
2018-09-04 11:03:47 +0800

    init

--- src.py
+++ src.py
@@ -206,7 +206,7 @@ class RawIme:
     def main():
         rawime = RawIme(base_path=path, model_weights=weights)
 
-        # rawime.train(lr=0.0000001, epochs=1, batch_size=233, load_w=1)
+        rawime.train(lr=0.0000001, epochs=1, batch_size=233, load_w=1)
 
         rawime.submit()
 
