1a48e7d48febc0ab5f998c809a8fe96b86745dab
mahy <992635910@qq.com>
2018-09-06 23:21:51 +0800

    init

--- image_embedding.py
+++ image_embedding.py
@@ -53,7 +53,7 @@ def raw_model(lr=0.001, shape=(64, 64, 3)):
     # for layer in base.layers[-7:]:
     #    layer.trainable = True
     output = layers.GlobalMaxPooling2D()(base.output)
-    output = layers.Dense(30, activation='sigmoid')(output)
+    output = layers.Dense(230, activation='sigmoid')(output)
     model = Model(inputs=inputs, outputs=output)
     opti = optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     model.compile(optimizer=opti, loss=losses.mean_squared_error)
@@ -61,7 +61,9 @@ def raw_model(lr=0.001, shape=(64, 64, 3)):
     for i, layer in enumerate(model.layers):
         print(i, layer.name)
 
-    middle_layer_model = Model(inputs=model.input, outputs=base.output)
+    output = layers.GlobalMaxPooling2D()(base.output)
+    output = layers.Dense(1024, activation='sigmoid')(output)
+    middle_layer_model = Model(inputs=model.input, outputs=output)
 
     return model, middle_layer_model
 
--- src.py
+++ src.py
@@ -1,6 +1,7 @@
 import numpy as np
 from data2array import data2array
 from image_embedding import ime_model, raw_model
+import xgboost as xgb
 import copy
 import keras
 
@@ -180,15 +181,76 @@ class RawIme:
         print(ev)
         return model_weights
 
+    def train_xgb(self):
+        data = data2array(self.base_path)
+        train_list = data['train_list']
+        train_num = 30000
+        val_num = 1000
+        x = []
+        y1, y2, y3 = [], [], []
+        for i in train_list:
+            x.append(train_list[i]['img_array'])
+            y2.append(train_list[i]['attributes_per_classf_330_num'])
+            # _y1 = np.zeros(230)
+            # _y1[train_list[i]['label_array']] = 1
+            _y1 = data['label_map'].index(train_list[i]['label'])
+            y1.append(_y1)
+            # y3.append(train_list[i]['label_real_name_class_wordembeddings'])
+        x = np.array(x)
+        y1 = np.array(y1)
+        model = self.model()
+        model_230 = model[0]
+        model_middle_layer = model[1]
+        model_230.load_weights(self.model_weights)
+        predict = model_middle_layer.predict(np.array(x))
+        params = {
+            'booster': 'gbtree',
+            # 这里手写数字是0-9，是一个多类的问题，因此采用了multisoft多分类器，
+            'objective': 'multi:softmax',
+            'num_class': 230,  # 类数，与 multisoftmax 并用
+            'gamma': 0.05,  # 在树的叶子节点下一个分区的最小损失，越大算法模型越保守 。[0:]
+            'max_depth': 230,  # 构建树的深度 [1:]
+            # 'lambda':450,  # L2 正则项权重
+            'subsample': 0.4,  # 采样训练数据，设置为0.5，随机选择一般的数据实例 (0:1]
+            'colsample_bytree': 0.7,  # 构建树树时的采样比率 (0:1]
+            # 'min_child_weight':12, # 节点的最少特征数
+            'silent': 1,
+            'eta': 0.05,  # 如同学习率
+            'seed': 710,
+            'nthread': 15,  # cpu 线程数,根据自己U的个数适当调整
+        }
+        plst = params
+        # Using 10000 rows for early stopping.
+        offset = 50000  # 训练集中数据60000，划分50000用作训练，10000用作验证
+
+        num_rounds = 200  # 迭代你次数
+
+
+        # 划分训练集与验证集
+        xgtrain = xgb.DMatrix(predict[:train_num], y1[:train_num])
+        xgval = xgb.DMatrix(predict[train_num:], label=y1[train_num:])
+
+        # return 训练和验证的错误率
+        watchlist = [(xgtrain, 'train'), (xgval, 'val')]
+
+        # training model
+        # early_stopping_rounds 当设置的迭代次数较大时，early_stopping_rounds 可在一定的迭代次数内准确率没有提升就停止训练
+        model_xgb = xgb.train(plst, xgtrain, num_rounds, watchlist, early_stopping_rounds=100)
+        # model.save_model('./model/xgb.model') # 用于存储训练出的模型
+        model_xgb.save_model('xgb.model')
+
+
     def submit(self):
         data = data2array(self.base_path)
         test_list_array = data['test_list_array']
         test_list_name = data['test_list_name']
         model = self.model()
         model_230 = model[0]
-        model = model[0]
+        model = model[1]
         model_230.load_weights(self.model_weights)
-        predict = model.predict(np.array(test_list_array))
+        predict = model[1].predict(np.array(test_list_array))
+        xgtest = xgb.DMatrix(predict)
+        preds = model.predict(xgtest, ntree_limit=model.best_iteration)
         submit_lines = []
         lable_index = predict.argmax(1)
         n = 0
@@ -206,11 +268,12 @@ class RawIme:
     def main():
         rawime = RawIme(base_path=path, model_weights=weights)
 
-        rawime.train(lr=0.0001, epochs=1, batch_size=233, load_w=0)
+        rawime.train(lr=0.0001, epochs=1, batch_size=233, load_w=1)
 
-        rawime.submit()
+        # rawime.submit()
 
 
 if __name__ == '__main__':
     rawime = RawIme(base_path=path, model_weights=weights)
-    rawime.main()
+    # rawime.main()
+    rawime.train_xgb()
